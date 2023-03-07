8db867476fb009e723af7cec75432102843e744e
Trololoshkin <1stverg@gmail.com>
2023-03-07 13:27:46 +0300

    Update PythonApplication2.py

--- PythonApplication2.py
+++ PythonApplication2.py
@@ -1,39 +1,66 @@
 import pandas as pd
 import numpy as np
 import tensorflow as tf
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
 
-def train_network(data_path='data.csv', train_test_split_ratio=0.8, window_size=10, batch_size=32, epochs=50, learning_rate=0.001, prediction_length=1, order_size=1, profit_margin=0.05):
-    # Загрузка и предобработка данных
-    data = pd.read_csv(data_path)
-    data = data[['Close']]
-    data = np.array(data).reshape(-1)
-    data = (data - np.min(data)) / (np.max(data) - np.min(data))
-    
-    # Разделение данных на обучающую и тестовую выборки
-    train_size = int(len(data) * train_test_split_ratio)
-    train_data = data[:train_size]
-    test_data = data[train_size:]
-    
-    # Создание окон для обучения нейронной сети
-    def create_window(data, window_size):
-        X = []
-        y = []
-        for i in range(len(data) - window_size - prediction_length + 1):
-            X.append(data[i:i+window_size])
-            y.append(data[i+window_size:i+window_size+prediction_length])
-        return np.array(X), np.array(y)
-    
-    X_train, y_train = create_window(train_data, window_size)
-    X_test, y_test = create_window(test_data, window_size)
-    
-    # Создание и обучение нейронной сети
-    model = tf.keras.Sequential([
-        tf.keras.layers.Dense(64, activation='relu', input_shape=(window_size,)),
-        tf.keras.layers.Dense(32, activation='relu'),
-        tf.keras.layers.Dense(prediction_length)
-    ])
-    
-    optimizer = tf.keras.optimizers.Adam(lr=learning_rate)
-    model.compile(optimizer=optimizer, loss='mse')
-    
-    model.fit(X_train, y_train, batch_size=batch_size, epochs=epochs, validation_data=(X_test, y_test
+# Параметры модели
+data_path = 'data.csv'
+train_test_split_ratio = 0.8
+window_size = 30
+batch_size = 32
+epochs = 50
+learning_rate = 0.001
+prediction_length = 1
+order_size = 1000
+profit_margin = 0.05
+
+# Загрузка данных
+data = pd.read_csv(data_path)
+data = data.dropna()
+data = data.drop('date', axis=1)
+scaler = StandardScaler()
+data = scaler.fit_transform(data)
+X, y = [], []
+for i in range(window_size, len(data)):
+    X.append(data[i-window_size:i])
+    y.append(data[i, 0])
+X, y = np.array(X), np.array(y)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=1-train_test_split_ratio, shuffle=False)
+
+# Создание модели
+model = tf.keras.Sequential([
+    tf.keras.layers.LSTM(128, activation='relu', return_sequences=True),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.LSTM(64, activation='relu'),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Dense(prediction_length)
+])
+model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=learning_rate), loss='mse')
+
+# Обучение модели
+model.fit(X_train, y_train, batch_size=batch_size, epochs=epochs)
+
+# Прогнозирование цены
+last_window = data[-window_size:]
+last_window = np.array(last_window).reshape(1, window_size, -1)
+prediction = model.predict(last_window)
+predicted_price = scaler.inverse_transform(prediction)[0, 0]
+
+# Открытие позиции
+position = None
+if predicted_price > data[-1, 0]:
+    position = 'LONG'
+    order_price = data[-1, 0] + profit_margin * data[-1, 0]
+    order_quantity = order_size // order_price
+elif predicted_price < data[-1, 0]:
+    position = 'SHORT'
+    order_price = data[-1, 0] - profit_margin * data[-1, 0]
+    order_quantity = order_size // order_price
+if position is not None:
+    print(f'Opening {position} position at price {order_price} with quantity {order_quantity}')
+
+# Закрытие позиции
+if position == 'LONG' and data[-1, 0] >= order_price:
+    profit = order_quantity * (data[-1, 0] - order_price)
+    print(f'Closing LONG position at
